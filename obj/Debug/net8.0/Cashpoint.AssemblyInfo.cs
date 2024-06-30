6fca4884ead19b0d995893f65f2cb15f0a5ad8be
SynI20n <leonid.shcherbakov.01@mail.ru>
2024-06-30 12:02:38 +0300

    prime numbers

--- obj/Debug/net8.0/Cashpoint.AssemblyInfo.cs
+++ obj/Debug/net8.0/Cashpoint.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cashpoint")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83bee3899d47fd43684b6ded30a169e494222901")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cashpoint")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cashpoint")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
